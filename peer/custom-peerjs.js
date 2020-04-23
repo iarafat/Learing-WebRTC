0b7c7f1a223bbb18be7dee3fb257d8b617c0a18d
iarafat <miyasinarafat@gmail.com>
2020-04-23 21:55:54 +0600

    working with peer.js connection setup

---
+++ peer/custom-peerjs.js
@@ -0,0 +1,92 @@
+var constraints = { audio: true, video: true };
+navigator.getWebcam = navigator.mediaDevices.getUserMedia;
+
+var peer = new Peer({
+    key: 'lwjd5qra8257b9',
+    debug: 3,
+    config: {
+        'iceServers': [
+            { url: 'stun:stun.l.google.com:19302' },
+            { url: 'stun:stun1.l.google.com:19302' },
+            { url: 'turn:numb.viagenie.ca', username: "lisa@learnfromlisa.com", credential: "22paris22" }
+        ]
+    }
+});
+
+peer.on('open', function (id) {
+    console.log('My peer ID is: ' + id);
+});
+
+// on open, set the peer id
+peer.on('open', function () {
+    $('#my-id').text(peer.id);
+});
+
+
+// peer call
+peer.on('call', function (call) {
+    // answer automaticlly for demo
+    call.answer(window.localStream);
+    stepThree(call);
+});
+
+// click hanler setup
+$(function () {
+    // call connection
+    $('#make-call').on('click', function () {
+        var call = peer.call($('#callto-id').val(), window.localStream);
+        stepThree(call);
+    });
+
+    // end call
+    $('#end-call').on('click', function () {
+        window.existingCall.close();
+        setpTwo();
+    });
+
+    // retry if getUserMedia fails
+    $('#step1-retry').on('click', function () {
+        $('#step1-error').hide();
+        setpOne();
+    })
+
+    // get things started
+    setpOne();
+});
+
+function setpOne() {
+    // get audio/video streem
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (streem) {
+            $('#my-video').prop('srcObject', streem);
+
+            window.localStream = streem;
+            setpTwo();
+        })
+        .catch(function (err) {
+            console.log(err)
+            $('#step1-error').show();
+        });
+}
+
+function setpTwo() {
+    // adjust the UI
+    $('#step1', '#step3').hide();
+    $('#step2').show();
+}
+
+function stepThree(call) {
+    // hang up on an existing call if present
+    if (window.existingCall) {
+        window.existingCall.close();
+    }
+
+    // wait for stream on the call, then setup peer video
+    call.on('stream', function (stream) {
+        $('#firend').prop('srcObject', streem);
+    });
+
+    $('#step1', '#step2').hide();
+    $('#step3').show();
+}
+
