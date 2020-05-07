e3e9accc5187a9ce673086238ac5ff99a348d6a5
iarafat <miyasinarafat@gmail.com>
2020-05-07 13:30:31 +0600

    completed step one of the codelabs webrtc course

--- codelabs/js/main.js
+++ codelabs/js/main.js
@@ -1,2 +1,35 @@
 'use strict';
 
+const mediaStreamConstrains = {
+    /*video: {
+        width: {
+            min: 1280
+        },
+        height: {
+            min: 720
+        }
+    },*/
+    video: true,
+    audio: true,
+};
+
+const localVideo = document.querySelector('video');
+
+let localStream;
+
+function gotLocalMediaStream(mediaStream) {
+    //console.log(mediaStream.getVideoTracks());
+
+    localStream = mediaStream;
+    localVideo.srcObject = mediaStream;
+}
+
+function handleLocalMediaStreamError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+}
+
+// init media stream
+navigator.mediaDevices
+    .getUserMedia(mediaStreamConstrains)
+    .then(gotLocalMediaStream)
+    .catch(handleLocalMediaStreamError);
